62cbac55820de95fe30980c60f8a4e135063378a
Satish Pokala <satishpokala124@gmail.com>
2022-02-11 00:17:22 +0530

    Added 'typing' to the Time class.

--- Time.py
+++ Time.py
@@ -9,23 +9,23 @@ class Time:
         self.set_hour(h)
         self.set_minute(m)
 
-    def set_hour(self, h):
+    def set_hour(self, h) -> None:
         if not 0 <= h < 24:
             raise AttributeError('Argument "h" is out of the expected range 0 to 23')
         self.hour = int(h)
 
-    def set_minute(self, m):
+    def set_minute(self, m) -> None:
         if not 0 <= m < 60:
             raise AttributeError('Argument "m" is out of the expected range 0 to 59')
         self.minute = int(m)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f'{str(self.hour).rjust(2, "0")}:{str(self.minute).rjust(2, "0")}'
 
-    def __eq__(self, other):
+    def __eq__(self, other) -> bool:
         return self.hour == other.hour and self.minute == other.minute
 
-    def __lt__(self, other):
+    def __lt__(self, other) -> bool:
         if not isinstance(other, Time):
             raise TypeError(f'Expected argument of type {type(Time)} but got {type(other)}')
         if self.hour > other.hour:
@@ -34,16 +34,16 @@ class Time:
             return self.minute < other.minute
         return True
 
-    def __le__(self, other):
+    def __le__(self, other) -> bool:
         return self == other or self < other
 
-    def __gt__(self, other):
+    def __gt__(self, other) -> bool:
         return not self <= other
 
-    def __ge__(self, other):
+    def __ge__(self, other) -> bool:
         return not self < other
 
-    def __sub__(self, other):
+    def __sub__(self, other) -> float:
         if not isinstance(other, Time):
             raise TypeError
         small_ts, big_ts = [self, other] if self < other else [other, self]
@@ -51,7 +51,7 @@ class Time:
         min_diff = big_ts.minute - small_ts.minute
         return hr_diff + (min_diff / 60)
 
-    def __add__(self, minutes: int):
+    def __add__(self, minutes: int) -> 'Time':
         if not isinstance(minutes, int):
             raise TypeError(f'Expected argument of type {type(int)} but got {type(minutes)}')
         if self.minute + minutes < 60:
