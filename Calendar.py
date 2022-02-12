fbea0c66c04ae9c88f7c7bdee98b621343c17b07
Satish Pokala <satishpokala124@gmail.com>
2022-02-12 23:27:10 +0530

    Added schedule_meeting function to Calendar class.

--- Calendar.py
+++ Calendar.py
@@ -2,13 +2,15 @@ from typing import List
 
 from Date import Date
 from Event import Event
+from Time import Time
+from TimeStamp import TimeStamp
 
 
 class Calendar:
     completed_events: List[Event] = []
     events: List[Event] = []
 
-    def __init__(self, events):
+    def __init__(self, events: List[Event]):
         self.events = events
         self.sort_events_start_time()
 
@@ -21,6 +23,8 @@ class Calendar:
         self.completed_events.sort(key=lambda event: event.end_timestamp)
 
     def add_event(self, event: Event) -> None:
+        if not isinstance(event, Event):
+            raise TypeError(f'Expected argument of type {type(Event)} but got {type(event)}')
         for i, ith_event in enumerate(self.events):
             if event.start_timestamp <= ith_event.start_timestamp:
                 self.events.insert(i, event)
@@ -29,13 +33,36 @@ class Calendar:
     def mark_event_as_done(self):
         pass
 
-    def schedule_meeting(self, other: 'Calendar', date: Date, duration: int):
-        events1 = filter(
-            lambda event: event.start_timestamp.date == date or event.end_timestamp.date == date,
-            self.events
-        )
-        events2 = filter(
-            lambda event: event.start_timestamp.date == date or event.end_timestamp.date == date,
-            other.events
-        )
-        pass
+    def schedule_meeting(self, other: 'Calendar', date: Date, duration: int) -> Event or int:
+        def filter_fun(e):
+            return e.start_timestamp.date == date or e.end_timestamp.date == date
+
+        events1 = list(filter(filter_fun, self.events))
+        events2 = list(filter(filter_fun, other.events))
+        events1.sort(key=lambda e: e.start_timestamp)
+        events2.sort(key=lambda e: e.start_timestamp)
+
+        time_line = [Time(0, 0) + i * 15 for i in range(24 * 4)]
+        empty_slot = 0
+        for time in time_line:
+            is_cal1_free = True
+            is_cal2_free = True
+            for event in events1:
+                if event.start_timestamp.time < time < event.end_timestamp.time:
+                    is_cal1_free = False
+                    break
+            for event in events2:
+                if event.start_timestamp.time < time < event.end_timestamp.time:
+                    is_cal2_free = False
+                    break
+            if is_cal1_free and is_cal2_free:
+                empty_slot += 15
+            else:
+                empty_slot = 0
+
+            if duration <= empty_slot:
+                start_timestamp = TimeStamp(date, time - duration)
+                end_timestamp = TimeStamp(date, time)
+                scheduled_event = Event(start_timestamp, end_timestamp)
+                return Event
+        return -1
