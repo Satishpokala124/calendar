637bfa6824529beb8cc3320762027e5ba083e09c
Satish Pokala <satishpokala124@gmail.com>
2022-05-01 23:57:07 +0530

    Added 'register' view to 'account.views.py'.

--- calendar-api/account/views.py
+++ calendar-api/account/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render, redirect
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.core.exceptions import ValidationError
+from django.contrib.auth.password_validation import validate_password
 
 from .models import User
 from django.db.models import Q
@@ -36,8 +38,46 @@ def user_login(request):
     return Response({'message': 'Logged In'}, status=status.HTTP_200_OK)
 
 
+@api_view(['POST'])
 def register(request):
-    pass
+    username = request.data['username']
+    firstname = request.data['firstname']
+    lastname = request.data['lastname']
+    email = request.data['email']
+    password = request.data['password']
+    cnf_password = request.data['cnf_password']
+
+    try:
+        User.username_validator(username)
+    except ValidationError:
+        return Response({'error': 'Invalid username'}, status=status.HTTP_400_BAD_REQUEST)
+
+    try:
+        validate_password(password)
+    except ValidationError:
+        return Response({'error': 'Weak password'}, status=status.HTTP_400_BAD_REQUEST)
+
+    is_existing_username = len(User.objects.filter(username=username)) > 0
+    if is_existing_username:
+        return Response({'error': 'Username already taken'}, status=status.HTTP_400_BAD_REQUEST)
+
+    is_existing_email = len(User.objects.filter(email=email)) > 0
+    if is_existing_username:
+        return Response({'error': 'Email already exists'}, status=status.HTTP_400_BAD_REQUEST)
+
+    if password != cnf_password:
+        return Response({'error': 'Password and Confirm Password mismatched'}, status=status.HTTP_400_BAD_REQUEST)
+
+    user = User.objects.create_user(
+        username=username,
+        firs_tname=firstname,
+        last_name=lastname,
+        email=email,
+        password=password
+    )
+    user.save()
+    auth.login(request, user)
+    return Response({'message': 'User created'}, status=status.HTTP_201_CREATED)
 
 
 @api_view(['GET'])
