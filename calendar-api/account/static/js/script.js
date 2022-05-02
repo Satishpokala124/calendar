64bde0113b601d6c33038754f6a7e89acd270a66
Satish Pokala <satishpokala124@gmail.com>
2022-05-02 23:49:32 +0530

    Added test for register.

--- calendar-api/account/static/js/script.js
+++ calendar-api/account/static/js/script.js
@@ -1,31 +1,40 @@
-const login = (() => {
-	console.log('Logging in');
-	fetch('http://127.0.0.1:8000/account/login/', {
-		method: 'POST',
-		headers: {
-      		'Content-Type': 'application/json'
-  		},
-		body: JSON.stringify({
-			username: 'admin@admin.com',
-			password: 'admin123'
-		})
-	})
-	.then(response => response.json())
-	.then(data => console.log(data))
-})
+const login = () => {
+  fetch('http://127.0.0.1:8000/account/login/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      username: 'admin@admin.com',
+      password: 'admin123',
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+};
 
-const logout = (() => {
-	console.log('Logging out');
-	fetch('http://127.0.0.1:8000/account/logout/')
-	.then(response => response.json())
-	.then(data => console.log(data))
-})
+const register = () => {
+  fetch('http://127.0.0.1:8000/account/register/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      username: 'satish1',
+      firstname: 'Satish',
+      lastname: 'Pokala',
+      email: 'satish@satish.com',
+      password: 'satish123',
+      cnf_password: 'satish123',
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+};
 
-
-//fetch('http://127.0.0.1:8000/users/',{
-//	headers: new Headers({
-//    	"Authorization": `Basic ${btoa('admin:admin')}`
-//  })
-//})
-//.then(response => response.json())
-//.then(data => console.log(data))
+const logout = () => {
+  console.log('Logging out');
+  fetch('http://127.0.0.1:8000/account/logout/')
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+};
