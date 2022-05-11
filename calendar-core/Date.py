017af2a25b00a763de501eea7260d98040b4789f
Satish Pokala <satishpokala124@gmail.com>
2022-05-11 23:42:47 +0530

    Modified function names in Date.py

--- calendar-core/Date.py
+++ calendar-core/Date.py
@@ -8,10 +8,10 @@ class Date:
 
     def __init__(self, day: int, month: int, year: int):
         self.month_days = None
-        self.setYear(year)
-        self.setMonthDays()
-        self.setMonth(month)
-        self.setDay(day)
+        self.set_year(year)
+        self.set_month_days()
+        self.set_month(month)
+        self.set_day(day)
 
     def __str__(self) -> str:
         d = str(self.d).rjust(2, '0')
@@ -56,10 +56,10 @@ class Date:
         if not isinstance(days, int):
             raise TypeError(f'Expected argument of type {type(1)} but got {type(days)}')
         new_date = copy(self)
-        while new_date.d + days > new_date.maxDaysInThisMonth():
-            days -= (new_date.maxDaysInThisMonth() - new_date.d)
+        while new_date.d + days > new_date.max_days_in_this_month():
+            days -= (new_date.max_days_in_this_month() - new_date.d)
             new_date.d = 0
-            new_date.m = new_date.getNextMonth()
+            new_date.m = new_date.get_next_month()
             new_date.y += 1 if new_date.m == 1 else 0
         new_date.d += days
         return new_date
@@ -69,118 +69,81 @@ class Date:
             new_date = copy(self)
             while new_date.d - other < 1:
                 other -= new_date.d
-                new_date.m = new_date.previousMonth()
+                new_date.m = new_date.previous_month()
                 new_date.y -= 1 if new_date.m == 12 else 0
-                new_date.d = new_date.maxDaysInThisMonth()
+                new_date.d = new_date.max_days_in_this_month()
             new_date.d -= other
             return new_date
         elif isinstance(other, Date):
             smaller_day, bigger_day = [self, other] if self < other else [other, self]
             if self.y == other.y:
-                return bigger_day.numberOfDaysFromYearStart() - smaller_day.numberOfDaysFromYearStart()
+                return bigger_day.days_from_year_start() - smaller_day.days_from_year_start()
             days_between_years = sum(
-                [Date(1, 1, i).numberOfDaysInThisYear() for i in range(smaller_day.y + 1, bigger_day.y)])
+                [Date(1, 1, i).days_in_this_year() for i in range(smaller_day.y + 1, bigger_day.y)])
             return (
-                    smaller_day.numberOfDaysRemainingInThisYear() +
+                    smaller_day.days_remaining_in_this_year() +
                     days_between_years +
-                    bigger_day.numberOfDaysFromYearStart()
+                    bigger_day.days_from_year_start()
                 )
         else:
             raise TypeError(f'Expected argument of type {type(self)} or {type(1)} but got {type(other)}')
 
-    def isLeapYear(self) -> bool:
+    def is_leap_year(self) -> bool:
         year = self.y
         if (not (year % 4) and year % 100) or (not (year % 400)):
             return True
         return False
 
-    def maxDaysInThisMonth(self) -> int:
+    def max_days_in_this_month(self) -> int:
         return self.month_days[self.m - 1]
 
-    def getDay(self) -> int:
+    def get_day(self) -> int:
         return self.d
 
-    def getMonth(self) -> int:
+    def get_month(self) -> int:
         return self.m
 
-    def getNextMonth(self) -> int:
+    def get_next_month(self) -> int:
         return self.m + 1 if self.m < 12 else 1
 
-    def getYear(self) -> int:
+    def get_year(self) -> int:
         return self.y
 
-    def setDay(self, day: int):
+    def set_day(self, day: int):
         if not isinstance(day, int):
             raise TypeError(f'Expected argument of type {type(1)} but got {type(day)}')
-        if not 1 <= day <= self.maxDaysInThisMonth():
-            raise AttributeError(f'Argument "day" is out of the expected range {1} to {self.maxDaysInThisMonth()}.')
+        if not 1 <= day <= self.max_days_in_this_month():
+            raise AttributeError(f'Argument "day" is out of the expected range {1} to {self.max_days_in_this_month()}.')
         self.d = day
 
-    def setMonth(self, month: int):
+    def set_month(self, month: int):
         if not isinstance(month, int):
             raise TypeError(f'Expected argument of type {type(1)} but got {type(month)}')
         if not 1 <= month <= 12:
             raise AttributeError(f'Argument "month" is out of the expected range 1 to 12.')
         self.m = month
 
-    def setYear(self, year: int) -> None:
+    def set_year(self, year: int) -> None:
         if not isinstance(year, int):
             raise TypeError(f'Expected argument of type {type(1)} but got {type(year)}')
         self.y = year
 
-    def setMonthDays(self):
+    def set_month_days(self):
         self.month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-        if self.isLeapYear():
+        if self.is_leap_year():
             self.month_days[1] = 29
 
-    def previousMonth(self):
+    def previous_month(self):
         return 12 if self.m == 1 else self.m - 1
 
-    def firstDayOfTheYear(self):
+    def first_day_of_the_year(self):
         return Date(1, 1, self.y)
 
-    def numberOfDaysFromYearStart(self) -> int:
+    def days_from_year_start(self) -> int:
         return sum(self.month_days[:self.m - 1]) + self.d
 
-    def numberOfDaysInThisYear(self):
-        return 366 if self.isLeapYear() else 365
+    def days_in_this_year(self):
+        return 366 if self.is_leap_year() else 365
 
-    def numberOfDaysRemainingInThisYear(self):
-        return self.numberOfDaysInThisYear() - self.numberOfDaysFromYearStart()
-
-
-if __name__ == '__main__':
-    print('Running tests :')
-    print(f'1: 1-1-2000 = 1-1-2000 : {Date(1, 1, 2000) == Date(1, 1, 2000)}')
-    print(f'2: 1-1-2000 = 1-1-2001 : {Date(1, 1, 2000) == Date(1, 1, 2001)}')
-    print(f'3: 1-1-2001 = 1-1-2000 : {Date(1, 1, 2001) == Date(1, 1, 2000)}')
-    print(f'4: 1-2-2000 = 1-1-2000 : {Date(1, 2, 2000) == Date(1, 1, 2000)}')
-    print(f'5: 1-1-2000 = 1-2-2000 : {Date(1, 1, 2000) == Date(1, 2, 2000)}')
-    print(f'6: 2-1-2000 = 1-1-2000 : {Date(2, 1, 2000) == Date(1, 1, 2000)}')
-    print(f'7: 1-1-2000 < 1-1-2000 : {Date(1, 1, 2000) < Date(1, 1, 2000)}')
-    print(f'8: 1-1-2000 < 1-1-2001 : {Date(1, 1, 2000) < Date(1, 1, 2001)}')
-    print(f'9: 1-1-2001 < 1-1-2000 : {Date(1, 1, 2001) < Date(1, 1, 2000)}')
-    print(f'10: 1-2-2000 < 1-1-2000 : {Date(1, 2, 2000) < Date(1, 1, 2000)}')
-    print(f'11: 1-1-2000 < 1-2-2000 : {Date(1, 1, 2000) < Date(1, 2, 2000)}')
-    print(f'12: 2-1-2000 < 1-1-2000 : {Date(2, 1, 2000) < Date(1, 1, 2000)}')
-    print(f'13: 1-1-2000 > 1-1-2000 : {Date(1, 1, 2000) > Date(1, 1, 2000)}')
-    print(f'14: 1-1-2000 > 1-1-2001 : {Date(1, 1, 2000) > Date(1, 1, 2001)}')
-    print(f'15: 1-1-2001 > 1-1-2000 : {Date(1, 1, 2001) > Date(1, 1, 2000)}')
-    print(f'16: 1-2-2000 > 1-1-2000 : {Date(1, 2, 2000) > Date(1, 1, 2000)}')
-    print(f'17: 1-1-2000 > 1-2-2000 : {Date(1, 1, 2000) > Date(1, 2, 2000)}')
-    print(f'18: 2-1-2000 > 1-1-2000 : {Date(2, 1, 2000) > Date(1, 1, 2000)}')
-    d1 = Date(1, 2, 2000)
-    print(d1 + 10)
-    print(d1 + 20)
-    print(d1 + 30)
-    print(d1 + 40)
-    print(d1 + 50)
-    print(d1 + 60)
-    d2 = Date(28, 1, 2000)
-    d3 = Date(1, 2, 2000)
-    d4 = Date(1, 1, 2001)
-    print(d2.isLeapYear())
-    print(d1.maxDaysInThisMonth())
-    print(d2 - d1)
-    print(d3 - d1)
-    print(d4 - d1)
+    def days_remaining_in_this_year(self):
+        return self.days_in_this_year() - self.days_from_year_start()
